582281247c59e2bbcd0772723d5c5b3babe85aa9
erickmorales999 <ematamoros679@gmail.com>
2026-06-02 23:12:36 -0600

    Fix .gitignore and remove generated CMake build artifacts

--- build/CMakeFiles/MiniDeliveryDash.dir/src/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MiniDeliveryDash.dir/src/main.cpp.o: \
-  /Users/kenrylirazavala/Desktop/Mini-Delivery-Dash-co/src/main.cpp \
-  /Users/kenrylirazavala/Desktop/Mini-Delivery-Dash-co/src/game/Game.h
